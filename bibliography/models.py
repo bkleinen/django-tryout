2d64c64ce2f2ae2cbef13fd379ad23b5bb7d1a69
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-06-26 23:32:41 +0200

    create model and add app bibliography

--- bibliography/models.py
+++ bibliography/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class BibTag(models.Model):
+    name = models.CharField(max_length=50)
+    
+
+class Author(models.Model):
+    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=59)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class BibEntry(models.Model):
+    class Meta:
+        ordering = ["title"]
+    title = models.CharField(max_length=200)
+    url = models.CharField(max_length=50)
+    type = models.CharField(max_length=50) 
+    pub_date = models.DateTimeField("date added")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(BibTag)
+    
+    def __str__(self):
+        return self.title
+
+
+
+
+
+    
--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bibliography.apps.BibliographyConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
