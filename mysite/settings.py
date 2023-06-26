317d76686faed2144a5fbd1c2ba51048f37e9c99
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-06-26 23:34:04 +0200

    add ,

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bibliography.apps.BibliographyConfig'
+    'bibliography.apps.BibliographyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
